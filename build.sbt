cc7c83e17c848e7e5ae8985e1eee0583900d39b2
brandonlmorris <brandon.morris95@gmail.com>
2016-10-18 08:05:18 -0500

    Initial commit
    Basic project structure; just a "Hello world!" app

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "Manscala"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+mainClass in (Compile, run) := Some("me.brandonlmorris.manscala.Manscala")
+    
---
+++ src/main/scala/me/brandonlmorris/manscala/Manscala.scala
@@ -0,0 +1,10 @@
+package me.brandonlmorris.manscala
+
+/**
+  * Created by bmorris on 10/18/16.
+  */
+object Manscala {
+  def main(args: Array[String]) = {
+    println("Hello, world!")
+  }
+}
